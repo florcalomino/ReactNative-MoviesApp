b6189eeceb5e188f5ff833bd3e74e26c715eca62
Florencia Calomino <fcalomino@inclusion.cloud>
2021-04-15 14:11:53 -0300

    Adjust styles

--- src/helpers/getColores.tsx
+++ src/helpers/getColores.tsx
@@ -1,26 +1,21 @@
-import ImageColors from 'react-native-image-colors';
+import ImageColors from "react-native-image-colors";
 
+export const getImageColors = async (uri: string) => {
+  const colors = await ImageColors.getColors(uri, {});
 
+  let primary;
+  let secondary;
 
-export const getImageColors = async( uri: string ) => {
-    
-    
-    const colors = await ImageColors.getColors(uri, {});
+  if (colors.platform === "android") {
+    // Access android properties
+    primary = colors.dominant;
+    secondary = colors.average;
+  } else {
+    // Access iOS properties
+    // e.g.
+    primary = colors.primary;
+    secondary = colors.secondary;
+  }
 
-    let primary;
-    let secondary;
-    
-    if (colors.platform === "android") {
-        // Access android properties
-        primary = colors.dominant;
-        secondary = colors.average;
-    } else {
-        // Access iOS properties
-        // e.g.
-        primary = colors.primary;
-        secondary = colors.secondary;
-    }
-
-    return [primary, secondary];
-
-}
+  return [primary, secondary];
+};
